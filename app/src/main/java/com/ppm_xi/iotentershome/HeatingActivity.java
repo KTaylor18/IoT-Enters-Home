1deeef425490903f10bdc12eedfb83b5561598a9
KTaylor18 <40864378+KTaylor18@users.noreply.github.com>
2019-01-30 17:09:18 +0000

    Finished initial lighting and heating interfaces
    The interfaces look at the Firebase database and check what is there. It then generates the buttons and required fields automatically based on what is collected.

--- app/src/main/java/com/ppm_xi/iotentershome/HeatingActivity.java
+++ app/src/main/java/com/ppm_xi/iotentershome/HeatingActivity.java
@@ -1,13 +1,134 @@
 package com.ppm_xi.iotentershome;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.LinkedList;
 
 public class HeatingActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase fDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lighting);
+        setContentView(R.layout.activity_heating);
+
+        //22.01.2019
+        //stackoverflow.com/questions/42998415/how-to-access-one-key-of-json-tree-from-firebase-realtime-database
+        //stackoverflow.com/questions/15082432/how-to-create-button-dynamically-in-android
+        //25.01.2019
+        //stackoverflow.com/questions/40366717/firebase-for-android-how-can-i-loop-through-a-child-for-each-child-x-do-y
+        //26.01.2019
+        //stackoverflow.com/questions/7919173/android-set-textview-textstyle-programmatically
+        //stackoverflow.com/questions/4602902/how-to-set-the-text-color-of-textview-in-code
+        //30.01.2019
+        //stackoverflow.com/questions/7631808/java-splitting-a-string-into-2-strings-based-on-a-delimiter
+        //stackoverflow.com/questions/7829034/java-how-to-check-if-a-string-is-a-part-of-any-linkedlist-element
+        //stackoverflow.com/questions/29883982/do-not-add-string-if-string-exists-arraylist
+        mAuth = FirebaseAuth.getInstance();
+        fDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = fDatabase.getReference();
+        final LinkedList<String> linkedList = new LinkedList<>();
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dSnapShot) {
+
+                LinearLayout box = (LinearLayout) findViewById(R.id.buttBox);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.
+                        WRAP_CONTENT);
+
+                for (final DataSnapshot aSnapShot : dSnapShot.getChildren()) {
+                    for (final DataSnapshot bSnapShot : aSnapShot.getChildren()) {
+                        for (final DataSnapshot cSnapShot : bSnapShot.getChildren()) {
+                            for (final DataSnapshot eSnapShot : cSnapShot.getChildren()) {
+                                if ((eSnapShot.getKey()).equals("Heating")) {
+                                    for (final DataSnapshot fSnapShot : eSnapShot.getChildren()) {
+                                        String roomName = fSnapShot.getKey();
+                                        String[] strings = roomName.split("-", 2);
+                                        String rName = strings[0];
+                                        if (!linkedList.contains(rName)){
+                                            linkedList.add(rName);
+                                            addLabel(box, layoutParams, 36, rName, true);
+                                        }
+                                        for (String r : linkedList) {
+                                            Toast.makeText(HeatingActivity.this, r, Toast.LENGTH_SHORT).show();
+                                            if (r.contains(strings[0])) {
+                                                String oName = strings[1];
+                                                addLabel(box, layoutParams, 30, oName, false);
+
+                                                String textToUse = fSnapShot.getValue().toString();
+                                                Button newButton = new Button(HeatingActivity.this);
+                                                newButton.setText(textToUse);
+
+                                                box.addView(newButton, layoutParams);
+
+                                                newButton.setOnClickListener(new View.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(View view) {
+                                                        if ((fSnapShot.getValue()).toString().equals("On")) {
+                                                            ref.child(aSnapShot.getKey()).child(bSnapShot.getKey()).
+                                                                    child(cSnapShot.getKey()).child(eSnapShot.getKey()).
+                                                                    child(fSnapShot.getKey()).setValue("Off");
+                                                        } else {
+                                                            ref.child(aSnapShot.getKey()).child(bSnapShot.getKey()).
+                                                                    child(cSnapShot.getKey()).child(eSnapShot.getKey()).
+                                                                    child(fSnapShot.getKey()).setValue("On");
+                                                        }
+                                                        Toast.makeText(HeatingActivity.this, "Updating...", Toast.LENGTH_SHORT).show();
+                                                        Intent intent = new Intent(getApplicationContext(), HeatingActivity.class);
+                                                        startActivity(intent);
+                                                    }
+                                                });
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void addLabel(LinearLayout box, LinearLayout.LayoutParams layoutParams, int textSize, String name, boolean bold) {
+        TextView newTextView = new TextView(HeatingActivity.this);
+        newTextView.setText(name);
+        newTextView.setTypeface(Typeface.MONOSPACE);
+        if (bold == true) {
+            newTextView.setTypeface(Typeface.DEFAULT_BOLD);
+        }
+        newTextView.setTextSize(textSize);
+        newTextView.setTextColor(Color.parseColor("#000000"));
+
+        box.addView(newTextView, layoutParams);
     }
 }
--- app/src/main/java/com/ppm_xi/iotentershome/LightingActivity.java
+++ app/src/main/java/com/ppm_xi/iotentershome/LightingActivity.java
@@ -23,6 +23,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.LinkedList;
+
 public class LightingActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
@@ -41,10 +43,14 @@ public class LightingActivity extends AppCompatActivity {
         //26.01.2019
         //stackoverflow.com/questions/7919173/android-set-textview-textstyle-programmatically
         //stackoverflow.com/questions/4602902/how-to-set-the-text-color-of-textview-in-code
+        //30.01.2019
+        //stackoverflow.com/questions/7631808/java-splitting-a-string-into-2-strings-based-on-a-delimiter
+        //stackoverflow.com/questions/7829034/java-how-to-check-if-a-string-is-a-part-of-any-linkedlist-element
+        //stackoverflow.com/questions/29883982/do-not-add-string-if-string-exists-arraylist
         mAuth = FirebaseAuth.getInstance();
         fDatabase = FirebaseDatabase.getInstance();
         final DatabaseReference ref = fDatabase.getReference();
-        final String uid = mAuth.getUid();
+        final LinkedList<String> linkedList = new LinkedList<>();
 
         ref.addValueEventListener(new ValueEventListener() {
             @Override
@@ -55,22 +61,26 @@ public class LightingActivity extends AppCompatActivity {
                         LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.
                         WRAP_CONTENT);
 
-                for (final DataSnapshot aSnapShot : dSnapShot.getChildren()){
+                for (final DataSnapshot aSnapShot : dSnapShot.getChildren()) {
                     for (final DataSnapshot bSnapShot : aSnapShot.getChildren()) {
                         for (final DataSnapshot cSnapShot : bSnapShot.getChildren()) {
                             for (final DataSnapshot eSnapShot : cSnapShot.getChildren()) {
-
-                                if ((eSnapShot.getKey()).equals("Lighting")){
+                                if ((eSnapShot.getKey()).equals("Lighting")) {
                                     for (final DataSnapshot fSnapShot : eSnapShot.getChildren()) {
                                         String roomName = fSnapShot.getKey();
-                                        addLabel(box, layoutParams, 36, roomName, true);
+                                        String[] strings = roomName.split("-", 2);
+                                        String rName = strings[0];
+                                        if (!linkedList.contains(rName)){
+                                            linkedList.add(rName);
+                                            addLabel(box, layoutParams, 36, rName, true);
+                                        }
+                                        for (String r : linkedList) {
+                                            Toast.makeText(LightingActivity.this, r, Toast.LENGTH_SHORT).show();
+                                            if (r.contains(strings[0])) {
+                                                String oName = strings[1];
+                                                addLabel(box, layoutParams, 30, oName, false);
 
-                                        for (final DataSnapshot gSnapShot : eSnapShot.getChildren()) {
-                                            for (final DataSnapshot hSnapShot : gSnapShot.getChildren()){
-                                                String objectName = hSnapShot.getKey();
-                                                addLabel(box, layoutParams, 30, objectName, false);
-
-                                                String textToUse = hSnapShot.getValue().toString();
+                                                String textToUse = fSnapShot.getValue().toString();
                                                 Button newButton = new Button(LightingActivity.this);
                                                 newButton.setText(textToUse);
 
@@ -79,16 +89,14 @@ public class LightingActivity extends AppCompatActivity {
                                                 newButton.setOnClickListener(new View.OnClickListener() {
                                                     @Override
                                                     public void onClick(View view) {
-                                                        if ((hSnapShot.getValue()).equals("On")){
+                                                        if ((fSnapShot.getValue()).toString().equals("On")) {
                                                             ref.child(aSnapShot.getKey()).child(bSnapShot.getKey()).
                                                                     child(cSnapShot.getKey()).child(eSnapShot.getKey()).
-                                                                    child(fSnapShot.getKey()).
-                                                                    child(hSnapShot.getKey()).setValue("Off");
+                                                                    child(fSnapShot.getKey()).setValue("Off");
                                                         } else {
                                                             ref.child(aSnapShot.getKey()).child(bSnapShot.getKey()).
                                                                     child(cSnapShot.getKey()).child(eSnapShot.getKey()).
-                                                                    child(fSnapShot.getKey()).
-                                                                    child(hSnapShot.getKey()).setValue("Off");
+                                                                    child(fSnapShot.getKey()).setValue("On");
                                                         }
                                                         Toast.makeText(LightingActivity.this, "Updating...", Toast.LENGTH_SHORT).show();
                                                         Intent intent = new Intent(getApplicationContext(), LightingActivity.class);
@@ -97,6 +105,7 @@ public class LightingActivity extends AppCompatActivity {
                                                 });
                                             }
                                         }
+
                                     }
                                 }
                             }
@@ -105,6 +114,8 @@ public class LightingActivity extends AppCompatActivity {
                 }
             }
 
+
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
@@ -112,11 +123,11 @@ public class LightingActivity extends AppCompatActivity {
         });
     }
 
-    public void addLabel(LinearLayout box, LinearLayout.LayoutParams layoutParams, int textSize, String name, boolean bold){
+    public void addLabel(LinearLayout box, LinearLayout.LayoutParams layoutParams, int textSize, String name, boolean bold) {
         TextView newTextView = new TextView(LightingActivity.this);
         newTextView.setText(name);
         newTextView.setTypeface(Typeface.MONOSPACE);
-        if (bold == true){
+        if (bold == true) {
             newTextView.setTypeface(Typeface.DEFAULT_BOLD);
         }
         newTextView.setTextSize(textSize);
